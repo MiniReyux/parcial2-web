1ed9dc8fbdc7df16940886a2a0bf7126589ecc6d
MiniReyux <ja.reyes2@uniandes.edu.co>
2025-05-18 18:58:48 -0500

    start ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,25 @@ import { EstudianteModule } from './estudiante/estudiante.module';
 import { ProfesorModule } from './profesor/profesor.module';
 import { ProyectoModule } from './proyecto/proyecto.module';
 import { EvaluacionModule } from './evaluacion/evaluacion.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EstudianteEntity } from './estudiante/estudiante.entity';
+import { ProfesorEntity } from './profesor/profesor.entity';
+import { ProyectoEntity } from './proyecto/proyecto.entity';
+import { EvaluacionEntity } from './evaluacion/evaluacion.entity';
 
 @Module({
-  imports: [EstudianteModule, ProfesorModule, ProyectoModule, EvaluacionModule],
+  imports: [TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'parcial2',
+      entities: [EstudianteEntity, ProfesorEntity, ProyectoEntity, EvaluacionEntity],
+      dropSchema: true,
+      synchronize: true,
+    }),
+    EstudianteModule, ProfesorModule, ProyectoModule, EvaluacionModule],
   controllers: [AppController],
   providers: [AppService],
 })
